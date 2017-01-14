e34c1a1f88d330dc8a1025b7f147112ce4b28816
Carlos Mendible <cmendible@outlook.com>
2017-01-14 13:06:28 +0100

    Fixed AssemblyProduct name

--- src/AspNetCore.Health.Ftp/Properties/AssemblyInfo.cs
+++ src/AspNetCore.Health.Ftp/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AspNetCore.Health.Amqp")]
+[assembly: AssemblyProduct("AspNetCore.Health.Ftp")]
 [assembly: AssemblyTrademark("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
