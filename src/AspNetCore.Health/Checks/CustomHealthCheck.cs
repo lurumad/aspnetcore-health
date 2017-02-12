89fe15193d471f9bc9f9909b2d559108dbbf9d9d
Carlos Mendible <cmendible@outlook.com>
2017-02-12 11:40:52 +0100

    Adding a custom health check

---
+++ src/AspNetCore.Health/Checks/CustomHealthCheck.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AspNetCore.Health.Checks
+{
+    public class CustomHealthCheck : HealthCheck
+    {
+        private readonly Func<Task<bool>> check;
+
+        public CustomHealthCheck(string serviceName, Func<Task<bool>> check) : base("Custom", serviceName)
+        {
+            this.check = check;
+        }
+
+        public override async Task CheckAsync(HealthCheckContext context)
+        {
+            try
+            {
+                context.AddResult(await check().ConfigureAwait(false) ? Healthy() : Unhealthy());
+            }
+            catch
+            {
+                context.AddResult(Unhealthy());
+            }
+        }
+    }
+}
