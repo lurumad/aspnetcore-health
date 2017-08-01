2f8e9d13ecec6a46d3aa4a217399ee9fa58512ab
Ryan Miller <Ryan.Miller@chrobinson.com>
2017-08-01 14:15:25 -0500

    keep the state of healthy

--- test/AspNetCore.Health.Tests/MiddlewareTests.cs
+++ test/AspNetCore.Health.Tests/MiddlewareTests.cs
@@ -32,6 +32,26 @@ namespace AspNetCore.Health.Tests
         }
 
         [Fact]
+        public async Task returns_a_httpstatuscode_internalservererror_if_there_are_any_unhealthy_service_with_atleast_one_healthy()
+        {
+            var builder = new WebHostBuilder()
+                .ConfigureServices(services =>
+                    {
+                        services.AddHealthChecks(context =>
+                        {
+                            context.AddUrlCheck("http://invalidurl");
+                            context.AddUrlCheck("http://www.google.com");
+                        });
+                    })
+                .Configure(app => app.UseHealthCheck("/health"));
+            var server = new TestServer(builder);
+
+            var response = await server.CreateClient().GetAsync("/health");
+
+            response.StatusCode.Should().Be(HttpStatusCode.InternalServerError);
+        }
+
+        [Fact]
         public async Task returns_a_httpstatuscode_ok_if_there_are_any_unhealthy_service()
         {
             var builder = new WebHostBuilder()
